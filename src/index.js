c4f98b4d0a9c45002fb82795d9e96bbdc0bb5d73
R-Layer <layer@europe.com>
2019-05-20 17:23:00 +0200

    implement UI

--- src/index.js
+++ src/index.js
@@ -1,101 +1,197 @@
-import jsonToCsv from './jsonToCsv';
-import csvToJson from './csvToJson';
+import convertJsonToCsv from './jsonToCsv';
+import convertCsvToJson from './csvToJson';
 
-import './index.css'
+import './index.css';
+import './index1024.css';
 
-const CSVPanel = document.querySelector('.left-panel');
-const loadCSV_btn = document.querySelector('#csv-loader');
-const displayCSV_textarea = document.querySelector('#csv-area');
-const CSVMetadata_input = document.querySelector('#csv-metadata');
+const csvPanel = document.querySelector('#panel-one');
+const jsonPanel = document.querySelector('#panel-two');
+const togglerBtn = document.querySelector('#toggler');
 
-const JSONPanel = document.querySelector('.right-panel');
-const loadJSON_btn = document.querySelector('#json-loader');
-const displayJSON_textarea = document.querySelector('#json-area');
-const JSONMetadata_input = document.querySelector('#json-metadata');
+const displayOptions = document.querySelector('#header-options');
+const swapAreasBtn = document.querySelector('#swap-areas');
+const splitHBtn = document.querySelector('#split-h');
+const splitVBtn = document.querySelector('#split-v');
 
-const indentation_btn = document.querySelector('#indentation');
-const toggler_btn = document.querySelector('#toggler');
-const convert_btn = document.querySelector('#convert-btn');
-const clear_btn = document.querySelector('#clear');
+const pageContainer = document.querySelector('#main-container');
 
-let currentConversion = 1; // 0 - json to csv, 1 -csv to json
+const saveCsvEditorBtn = document.querySelector('#panel-one #save-btn');
+const saveJsonEditorBtn = document.querySelector('#panel-two #save-btn');
 
-function handleFileload(e){
-    let area, metadata, mimeTypes;
+const loadCsvBtn = document.querySelector('#panel-one #load-btn');
+const loadJsonBtn = document.querySelector('#panel-two #load-btn');
 
-    if(e.target.id === 'csv-loader'){
-        area = displayCSV_textarea;
-        metadata = CSVMetadata_input;
-        mimeTypes = ['text/csv', 'text/tsv'];
-    } else {
-        area = displayJSON_textarea;
-        metadata = JSONMetadata_input;
-        mimeTypes = ['application/json'];
-    }
+const beautifyCsvBtn = document.querySelector('#panel-one #beautify');
+const beautifyJsonBtn = document.querySelector('#panel-two #beautify');
 
-    const file = e.target.files[0];
-    const reader = new FileReader();
+const csvEditorArea = document.querySelector('#panel-one #upper-area');
+const jsonEditorArea = document.querySelector('#panel-two #upper-area');
 
-    reader.onload = evt => {
-        area.value = evt.target.result;
-        metadata.value = `File name: ${file.name} - File size: ${file.size}`;
-    }
+const closeCsvEditorBtn = document.querySelector('#panel-one #close-upper-area');
+const closeJsonEditorBtn = document.querySelector('#panel-two #close-upper-area');
 
-    if(!mimeTypes.includes(file.type)) {
-        console.log('file not supported!');
-        return;
-    }
-    reader.readAsText(file)
-}
+const csvEditorSplitArea = document.querySelector('#panel-one .split-area');
+const jsonEditorSplitArea = document.querySelector('#panel-two .split-area');
 
-// Paul sasik [STACK OVERFLOW]
-function prettyPrint(){
-    const rawJson = displayJSON_textarea.value;
-    const cleanJson = JSON.stringify(JSON.parse(rawJson), undefined, 2);
-    displayJSON_textarea.value = cleanJson;
+const csvEditorLowerArea = document.querySelector('#panel-one  #lower-area');
+const jsonEditorLowerArea = document.querySelector('#panel-two #lower-area');
+
+const csvEditorLowerWrapperArea = document.querySelector('#panel-one #lower-area-wrapper');
+const jsonEditorLowerWrapperArea = document.querySelector('#panel-two #lower-area-wrapper');
+
+const closeCsvEditorLowerArea = document.querySelector('#panel-one #close-lower-area');
+const closeJsonEditorLowerArea = document.querySelector('#panel-two #close-lower-area');
+
+const csvMetadataInput = document.querySelector('#panel-one #metadata');
+const jsonMetadataInput = document.querySelector('#panel-two #metadata');
+
+const convertBtn = document.querySelector('#convert-btn');
+
+
+let csvToJson = true; 
+let conversionState = 0;
+/* 
+class inverse change flex flow AND column position
+class active change ONLY column position
+ */
+function reverse(){
+    csvToJson = !csvToJson;
+    csvPanel.classList.toggle('inverse');
+    jsonPanel.classList.toggle('inverse');
+    csvEditorArea.value = "";
+    jsonEditorArea.value = "";
 };
 
-function clear(){
-    clearCsv();
-    clearJson();
-}
-
-function clearCsv(){
-    displayCSV_textarea.value = "";
-    CSVMetadata_input.value = "";
-}
-
-function clearJson(){
-    displayJSON_textarea.value = "";
-    JSONMetadata_input.value = "";
-}
-
-function toggleConversion(){
-    if(currentConversion) {
-        currentConversion = 0;
-        JSONPanel.classList.add("active");
-        loadJSON_btn.disabled = false;
-        displayJSON_textarea.disabled = false;
-        CSVPanel.classList.remove("active");
-        loadCSV_btn.disabled = true;
-        displayCSV_textarea.disabled = true;
-        toggler_btn.innerText =  "JSON to CSV (click to toggle)"
-    } else {
-        currentConversion = 1;
-        CSVPanel.classList.add("active");
-        loadCSV_btn.disabled = false;
-        displayCSV_textarea.disabled = false;
-        JSONPanel.classList.remove("active");
-        loadJSON_btn.disabled = true;
-        displayJSON_textarea.disabled = true;
-        toggler_btn.innerText =  "CSV to JSON (click to toggle)"
+function convert(){
+    if(window.innerWidth < 1024) {
+        if(csvToJson) {
+            csvPanel.classList.toggle('inverse');
+            jsonPanel.classList.toggle('active');
+            closeJsonEditorBtn.classList.toggle('hidden');
+            saveJsonEditorBtn.disabled = jsonEditorArea.value === "";
+        } else {
+            jsonPanel.classList.toggle('inverse');
+            csvPanel.classList.toggle('active');
+            closeCsvEditorBtn.classList.toggle('hidden');
+            saveCsvEditorBtn.disabled = csvEditorArea.value === "";
+        }
+ 
+        displayOptions.classList.toggle('hidden');
+        convertBtn.classList.toggle('hidden');
+        toggler.disabled = !toggler.disabled;
     }
+
+
+    if(conversionState === 1) {
+        csvToJson
+            ?   resetJsonPanel()
+            :   resetCsvPanel();
+        conversionState = 0;
+    } else {
+        conversionState = 1;
+    }
+};
+
+function swapResultOriginal(){
+    if(csvToJson) {
+        csvPanel.classList.toggle('inverse');
+        jsonPanel.classList.toggle('active');
+        removeJsonPanelSplit();
+    } else {
+        jsonPanel.classList.toggle('inverse');
+        csvPanel.classList.toggle('active');
+        removeCsvPanelSplit();
+    }
+};
+
+function splitAreaVertically(){
+    if(csvToJson) {
+        if(jsonEditorSplitArea.classList.contains('h')) {
+            jsonEditorSplitArea.classList.remove('h')
+        } else {
+            jsonEditorLowerWrapperArea.classList.toggle('hidden');
+        }
+    } else {
+        if(csvEditorSplitArea.classList.contains('h')) {
+            csvEditorSplitArea.classList.remove('h')
+        } else {
+            csvEditorLowerWrapperArea.classList.toggle('hidden');
+        }
+    }
+};
+
+function splitAreaHorizontally(){
+    if(csvToJson) {
+        // code area already splitted vertically
+        if(!jsonEditorLowerWrapperArea.classList.contains('hidden') &&
+          !jsonEditorSplitArea.classList.contains('h')) {
+            jsonEditorSplitArea.classList.add('h');
+        } else {
+            jsonEditorSplitArea.classList.toggle('h');
+            jsonEditorLowerWrapperArea.classList.toggle('hidden');
+        }
+    } else {
+        // code area already splitted vertically
+        if(!csvEditorLowerWrapperArea.classList.contains('hidden') &&
+          !csvEditorSplitArea.classList.contains('h')) {
+            csvEditorSplitArea.classList.add('h');
+        } else {
+            csvEditorSplitArea.classList.toggle('h');
+            csvEditorLowerWrapperArea.classList.toggle('hidden');
+        }
+    }
+};
+
+function checkIfEmpty(e){
+    if(csvToJson) {
+        saveJsonEditorBtn.disabled = e.target.value === "";
+    } else {
+        saveCsvEditorBtn.disabled = e.target.value === "";
+    }
+
+};
+
+function resetJsonPanel(){
+    jsonEditorArea.value = "";
+    removeCsvPanelSplit();
+}
+
+function resetCsvPanel(){
+    csvEditorArea.value = "";
+    removeCsvPanelSplit();
+}
+
+function removeJsonPanelSplit(){
+    jsonEditorSplitArea.classList.remove('h');
+    jsonEditorLowerWrapperArea.classList.add('hidden');
+}
+
+function removeCsvPanelSplit(){
+    csvEditorSplitArea.classList.remove('h');
+    csvEditorLowerWrapperArea.classList.add('hidden');
 }
 
 
-loadCSV_btn.addEventListener('change', handleFileload);
-loadJSON_btn.addEventListener('change', handleFileload);
+function checkVal(){
+    console.log(isAreaEmpty);
+};
 
-indentation_btn.addEventListener('click', prettyPrint);
-toggler_btn.addEventListener('click', toggleConversion);
-clear_btn.addEventListener('click', clear);
+jsonEditorArea.addEventListener('input', checkIfEmpty);
+csvEditorArea.addEventListener('input', checkIfEmpty);
+
+beautifyJsonBtn.addEventListener('click', checkVal)
+beautifyCsvBtn.addEventListener('click', checkVal);
+
+togglerBtn.addEventListener('click', reverse);
+convertBtn.addEventListener('click', convert);
+
+swapAreasBtn.addEventListener('click', swapResultOriginal);
+
+closeJsonEditorBtn.addEventListener('click', convert);
+closeCsvEditorBtn.addEventListener('click', convert);
+
+splitVBtn.addEventListener('click', splitAreaVertically);
+splitHBtn.addEventListener('click', splitAreaHorizontally);
+
+closeCsvEditorLowerArea.addEventListener('click', removeCsvPanelSplit);
+closeJsonEditorLowerArea.addEventListener('click', removeJsonPanelSplit);
